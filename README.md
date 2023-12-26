fae7cd3a43ccb6d9cd43280ccd627fb53ba2689a
zhaoxt <zhaoxt@greatld.com>
2023-12-26 15:51:18 +0800

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # padding-oracle-attack
 
 分组密码CBC填充提示攻击Node.js演示代码
+
+- 点击查看[CBC模式](https://zh.wikipedia.org/wiki/%E5%88%86%E7%BB%84%E5%AF%86%E7%A0%81%E5%B7%A5%E4%BD%9C%E6%A8%A1%E5%BC%8F#%E5%AF%86%E7%A0%81%E5%9D%97%E9%93%BE%E6%8E%A5%EF%BC%88CBC%EF%BC%89)
+
+- 点击查看[填充提示攻击](https://zh.wikipedia.org/wiki/%E5%AF%86%E6%96%87%E5%A1%AB%E5%A1%9E%E6%94%BB%E5%87%BB)
+
+这里以`aes-128-cbc`加密算法做演示，用`node.js`实现了针对密文的填充提示攻击，在无需知道密钥的前提下，解读明文。
--- main.js
+++ main.js
@@ -1,88 +1,11 @@
 /**
  * 填充提示攻击破解CBC模式
  */
-const crypto = require('crypto')
+
 const _ = require('lodash')
-const { xorBuffer, skipLine } = require('./lib')
+const { skipLine } = require('./lib')
 const { createCrypto } = require('./crypto')
-
-/**
- * 应该填充的Buffer
- */
-const getPaddingBuf = (n, lessOneByte = false) => {
-  const loop = lessOneByte & n > 0 ? n - 1 : n
-  const buffer = Buffer.alloc(loop)
-  for (let i = 0; i < loop; i++) {
-    buffer.writeUInt8(n, i)
-  }
-  return buffer
-}
-
-/**
- * 填充提示攻击的IV生成器
- * { byteSize: 16, testNum: 1, testPos: -1 }
- * => <Buffer 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01>
- * { byteSize: 16, testNum: 1, testPos: -2 }
- * => <Buffer 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00>
- * { byteSize: 16, testNum: 200, testPos: -1 }
- * => <Buffer 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 c8>
- * { byteSize: 16, tailBuffer: Buffer.from('123456', 'hex'), testNum: 1, testPos: -4 }
- * => <Buffer 00 00 00 00 00 00 00 00 00 00 00 00 01 12 34 56>
- */
-const getTestIVBuffer = ({ byteSize, tailBuffer, testNum, testPos }) => {
-  const buffer = Buffer.alloc(byteSize)
-  if (tailBuffer) {
-    const offset = byteSize - Buffer.byteLength(tailBuffer)
-    buffer.write(tailBuffer.toString('hex'), offset, Buffer.byteLength(tailBuffer), 'hex')
-  }
-  buffer.writeUInt8(testNum, byteSize + testPos)
-  return buffer
-}
-
-/**
- * 填充提示攻击代码示例
- * 已知初始化向量, 密文, 可利用解密函数
- */
-const paddingOracleAttack = (cipherBlock, ivHex, decrypt) => {
-  let tailBuffer, interBuf
-  for (let n = 0; n < 16; n++) {
-    console.log('')
-    console.log('====================')
-    console.log(`破解最后第${n}个字节`)
-    const paddingBuf = getPaddingBuf(n + 1)
-    console.log('本轮明文应该填充的字节:', paddingBuf.toString('hex'))
-    for (let i = 0; i < 256; i++) {
-      // 生成用于撞击的IV
-      const testIVBuffer = getTestIVBuffer({
-        byteSize: 16,
-        tailBuffer,
-        testNum: i,
-        testPos: -n - 1
-      })
-      const ivTestHex = testIVBuffer.toString('hex')
-      try {
-        // 尝试解密
-        decrypt(cipherBlock, ivTestHex)
-        const buf = Buffer.from(ivTestHex, 'hex').subarray(16 - n - 1)
-        console.log('解密成功的IV:', ivTestHex, buf)
-
-        interBuf = xorBuffer(buf, paddingBuf)
-        console.log('中间值:', interBuf)
-
-        const nextPaddingBufLessOneByte = getPaddingBuf(n + 2, true)
-        console.log('nextPaddingBufLessOneByte', nextPaddingBufLessOneByte)
-        tailBuffer = xorBuffer(interBuf, nextPaddingBufLessOneByte)
-        console.log('下一轮起始IV', tailBuffer)
-        // console.log()
-        // f(53) @ 07 = 01
-        break
-      } catch (e) {
-        // 抛错忽略
-      }
-    }
-  }
-  return xorBuffer(interBuf, Buffer.from(ivHex, 'hex')).toString('hex')
-}
+const { paddingOracleAttack } = require('./attack')
 
 const test = () => {
   const algorithm = 'aes-128-cbc'
